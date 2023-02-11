9f69cc0d3c8456b2f6d083c175a10f7f20125db0
Savannah Yost <savannahyost85@gmail.com>
2023-02-11 13:48:32 -0500

    XML activity added

--- app/src/main/java/com/example/lightningandroidtalk/UI/PartsOfAppManifest.java
+++ app/src/main/java/com/example/lightningandroidtalk/UI/PartsOfAppManifest.java
@@ -2,7 +2,10 @@ package com.example.lightningandroidtalk.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lightningandroidtalk.R;
 
@@ -12,5 +15,14 @@ public class PartsOfAppManifest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parts_of_app_manifest);
+
+        Button next = findViewById(R.id.nextXML);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartsOfAppManifest.this, PartsOfXML.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lightningandroidtalk/UI/PartsOfXML.java
@@ -0,0 +1,16 @@
+package com.example.lightningandroidtalk.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lightningandroidtalk.R;
+
+public class PartsOfXML extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parts_of_xml);
+    }
+}
