5c85ad1d964e3066d7306eaf191d8cfb06b0e8ca
Savannah Yost <savannahyost85@gmail.com>
2023-02-10 19:09:47 -0500

    added main

--- app/src/main/java/com/example/lightningandroidtalk/UI/MainActivity.java
+++ app/src/main/java/com/example/lightningandroidtalk/UI/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.lightningandroidtalk;
+package com.example.lightningandroidtalk.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.lightningandroidtalk.R;
 
 public class MainActivity extends AppCompatActivity {
 
