178b3100b9a7ba77f01ef6586c06b2f223008e6a
Savannah Yost <savannahyost85@gmail.com>
2023-02-10 21:12:03 -0500

    added next page

--- app/src/main/java/com/example/lightningandroidtalk/UI/MainActivity.java
+++ app/src/main/java/com/example/lightningandroidtalk/UI/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.lightningandroidtalk.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lightningandroidtalk.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PartsOfAppManifest.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/lightningandroidtalk/UI/PartsOfAppManifest.java
+++ app/src/main/java/com/example/lightningandroidtalk/UI/PartsOfAppManifest.java
@@ -1,4 +1,16 @@
 package com.example.lightningandroidtalk.UI;
 
-public class PartsOfAppManifest {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lightningandroidtalk.R;
+
+public class PartsOfAppManifest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parts_of_app_manifest);
+    }
 }
