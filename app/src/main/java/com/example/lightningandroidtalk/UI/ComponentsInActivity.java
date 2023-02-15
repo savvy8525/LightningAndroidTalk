3a8e1887b47041cdde6f6d97e3d74dc29683f9e6
Savannah Yost <savannahyost85@gmail.com>
2023-02-14 21:40:06 -0500

    finish delete

--- app/src/main/java/com/example/lightningandroidtalk/UI/ComponentsInActivity.java
+++ app/src/main/java/com/example/lightningandroidtalk/UI/ComponentsInActivity.java
@@ -7,11 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.example.lightningandroidtalk.R;
@@ -19,9 +23,13 @@ import com.example.lightningandroidtalk.R;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.TooManyListenersException;
 
 public class ComponentsInActivity extends AppCompatActivity {
 
+    private EditText editText;
+    String editTextString;
     ComponentAdapter componentAdapter;
     List<String> components = new ArrayList<>(Arrays.asList("TextView", "Button", "Switch", "Spinner"));
 
@@ -32,7 +40,10 @@ public class ComponentsInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_components_in);
 
-        System.out.println(components);
+        editText = findViewById(R.id.editText);
+        editTextString = editText.getText().toString();
+
+//        System.out.println(components);
 
         RecyclerView rv = findViewById(R.id.componentRV);
         componentAdapter = new ComponentAdapter(this);
@@ -45,9 +56,6 @@ public class ComponentsInActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-//        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.switch1);
-//        SwitchCompat switchCompat = (SwitchCompat) findViewById(R.id.switch1);
 
         Switch switch1 = (Switch) findViewById(R.id.switch1);
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -63,10 +71,65 @@ public class ComponentsInActivity extends AppCompatActivity {
             }
         });
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        RecyclerView rv = findViewById(R.id.componentRV);
+        componentAdapter = new ComponentAdapter(this);
+        rv.setAdapter(componentAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
 
+        componentAdapter.setListItems(components);
+    }
+
+    public void addToRV(String string) {
+        if(string != null) {
+            components.add(string);
+        }
+
+    }
+
+    public void deleteFromRV(String string) {
+        if(string != null) {
+
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_components_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add:
+                editText= findViewById(R.id.editText);
+                editTextString = editText.getText().toString();
+                    addToRV(editTextString);
+                componentAdapter.setListItems(components);
+                return true;
+            case R.id.delete:
+                editText= findViewById(R.id.editText);
+                editTextString = editText.toString();
+                    deleteFromRV(editTextString);
+                    componentAdapter.setListItems(components);
+                    System.out.println(components);
+                return true;
+            case R.id.toast:
+                Toast.makeText(this, "Thank you for listening!", Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return false;
+    }
 
 
 
